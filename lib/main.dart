c69c075aefe9dd1011eef62c7fa8a562832a2624
Receppzz <receppzz60@gmail.com>
2024-05-05 22:34:11 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WidgetDetail());
+}
+
+class WidgetDetail extends StatefulWidget {
+  const WidgetDetail({super.key});
+
+  @override
+  State<WidgetDetail> createState() => _WidgetDetailState();
+}
+
+class _WidgetDetailState extends State<WidgetDetail> {
+  int value = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Widget Detail"),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange,
+        leading: const Text("Menu"),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage("assets/images/image-1.jpg"),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            value = value + 1;
+          });
+        },
+        child: const Text("+"),
+      ),
+    ));
+  }
+}
